339b0c1c47d6176e8a6aa5644078ce909d4b1810
MarvRoldan <marvinroldan_@hotmail.com>
2020-03-10 11:58:32 -0600

    Added random words

---
+++ js/scripts.js
@@ -0,0 +1,16 @@
+// Bunch of random words.
+var randomWords = [
+    "cook",
+    "kid",
+    "telling",
+    "explore",
+    "tray"
+]; 
+
+// Pick from "randomWords"
+var wordToGuess = randomWords[Math.floor(Math.random()*randomWords.length)];
+
+alert(wordToGuess);
+
+
+
