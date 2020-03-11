7249dea154b65a088f8af20144d203f3514ba231
MarvRoldan <marvinroldan_@hotmail.com>
2020-03-11 09:03:41 -0600

    Cleaned up a few things

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,42 @@
-// Bunch of random words.
-var randomWords = [
-    "cook",
-    "kid",
-    "telling",
-    "explore",
-    "tray"
-]; 
+var randomWords = [ "fastidious", "cake", "available", "conflict", "helmet" ]; 
+var wordToGuess = randomWords[ Math.floor(Math.random()*randomWords.length) ];
+var wordCharacters = [];
 
-// Pick from "randomWords"
-var wordToGuess = randomWords[Math.floor(Math.random()*randomWords.length)];
+console.log( "Enter hangMan(); to start!" );
 
-alert(wordToGuess);
+function hangMan() {
+    for( i = 0; i < wordToGuess.length; i++ ) {
+        wordCharacters[i] = "_";
+    }
+    console.log( wordCharacters );
+}
 
+// MAKE FUNCTION TO GET USER TO INPUT A LETTER.
 
+function check ( guess ) {
+    var rightGuesses = 0;
+
+    for ( i = 0; i < wordToGuess.length; i++) {
+        if (wordToGuess[i] === guess) {
+            wordCharacters[i] = guess;
+            rightGuesses++;
+        }
+
+        // CAN WRITE FUNCTION TO CHECK IF GUESS IS WRONG. ADD STRIKES HERE.
+    }
+}
+
+function storeLetter( guess ) {
+    var inputtedGuess = false;
+
+    for ( i = 0; i < wordToGuess.length; i++ ) {
+        if( guess === wordToGuess[i] ) 
+        {
+            inputtedGuess = true;
+        }
+        if (inputtedGuess === false) {
+            wordToGuess.push(guess);
+        }
+    }
+}
 
