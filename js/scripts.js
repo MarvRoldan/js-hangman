60f2a954e3cb12e83fa569e5d079308f38e35a5d
MarvRoldan <marvinroldan_@hotmail.com>
2020-03-10 23:35:43 -0600

    Added stored the guess

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,26 @@
 var randomWords = [ "fastidious", "cake", "available", "conflict", "helmet" ]; 
-var wordToGuess = randomWords[Math.floor(Math.random()*randomWords.length)];
+var wordToGuess = randomWords[ Math.floor(Math.random()*randomWords.length) ];
 var wordCharacters = [];
 
-console.log("Type hangMan(); to start!");
+console.log( "Enter hangMan(); to start!" );
 
 function hangMan() {
-    for( var i = 0; i < wordToGuess.length; i++ ) {
+    for( i = 0; i < wordToGuess.length; i++ ) {
         wordCharacters[i] = "_";
     }
-    console.log(wordCharacters);
+    console.log( wordCharacters );
 }
 
+function storeLetter( guess ) {
+    var inputtedGuess = false;
 
+    for ( i = 0; i < wordToGuess.length; i++ ) {
+        if( guess === wordToGuess[i] ) 
+        {
+            inputtedGuess = true;
+        }
+        if (inputtedGuess === false) {
+            wordToGuess.push(guess);
+        }
+    }
+}
