290bf208fe40d2b2589465b7e9bfd04dd6b24e32
MarvRoldan <marvinroldan_@hotmail.com>
2020-03-11 22:11:42 -0600

    Added to the html

--- js/scripts.js
+++ js/scripts.js
@@ -1,66 +1,48 @@
-var randomWords = [ "fastidious", "cake", "available", "conflict", "helmet" ]; 
-var wordToGuess = randomWords[ Math.floor(Math.random()*randomWords.length) ];
-var wordCharacters = [];
+var guess; //user guess
+var letters = []; //correctly guessed letters
+var wrongLetters = []; //incorrectly guessed letters
+var counter = 0; //counts correct letters
+var lives; //counts users lives
 
-console.log( "Enter hangMan(); to start!" );
+var guessWords = ["crossing", "organ", "past", "green", "insight"];
 
-function hangMan() {
-    for( i = 0; i < wordToGuess.length; i++ ) {
-        wordCharacters[i] = "_";
-    }
-    console.log( wordCharacters );
+
+var randomWord = guessWords[Math.floor(Math.random() * guessWords.length)];
+
+//choosen word is replaced with
+function start() {
+  for (i = 0; i < randomWord.length; i++) {
+    letters[i] = "__";
+  }
+
+  document.getElementById("hidden").innerHTML = letters.join(" ");
+  console.log(randomWord);
 }
 
-// MAKE FUNCTION TO GET USER TO INPUT A LETTER.
-
-function check ( guess ) {
-    var rightGuesses = 0;
-
-    for ( i = 0; i < wordToGuess.length; i++) {
-        if (wordToGuess[i] === guess) {
-            wordCharacters[i] = guess;
-            rightGuesses++;
-        }
-
-        // CAN WRITE FUNCTION TO CHECK IF GUESS IS WRONG. ADD STRIKES HERE.
+function checkLetter() {
+  document.onkeyup = function(event) {
+    guess = event.key.toLowerCase();
+    var found = false; //lets use bool to check if a letter was found
+    for (i = 0; i < randomWord.length; i++) {
+      if (guess === randomWord[i]) {
+        letters[i] = guess;
+        document.getElementById("hidden").innerHTML = letters.join(" ");
+        found = true;
+      }
     }
+    //now all letters have been checked, was any found
+    if (found) return; //if yes return
+    
+    //wrong, lets also see if 
+    //it's not already logged, if not add it
+    //you could also then use  wrongLetters.length
+    //for working out if gueses area all used up.
+    if (wrongLetters.indexOf(guess) < 0) {
+      wrongLetters.push(guess);
+      document.getElementById("wrongGuess").innerHTML = wrongLetters.join(" ");
+    }
+  }
 }
 
-function storeLetter( guess ) {
-    var inputtedGuess = false;
-
-    for ( i = 0; i < wordToGuess.length; i++ ) {
-        if( guess === wordToGuess[i] ) 
-        {
-            inputtedGuess = true;
-        }
-        if (inputtedGuess === false) {
-            wordToGuess.push(guess);
-        }
-    }
-}
-
-    for ( i = 0; i < wordToGuess.length; i++) {
-        if (wordToGuess[i] === guess) {
-            wordCharacters[i] = guess;
-            rightGuesses++;
-        }
-
-        // CAN WRITE FUNCTION TO CHECK IF GUESS IS WRONG. ADD STRIKES HERE.
-    }
-}
-
-function storeLetter( guess ) {
-    var inputtedGuess = false;
-
-    for ( i = 0; i < wordToGuess.length; i++ ) {
-        if( guess === wordToGuess[i] ) 
-        {
-            inputtedGuess = true;
-        }
-        if (inputtedGuess === false) {
-            wordToGuess.push(guess);
-        }
-    }
-}
-
+start();
+checkLetter();
